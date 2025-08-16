ace7481a7028e7104f5fe0efe57724207a96b59e
Eduardo Salazar <eduardosalazar89@hotmail.es>
2025-08-15 23:06:20 -0600

    fix: update vue resolver export

--- packages/vue-resolver/types/index.d.ts
+++ packages/vue-resolver/types/index.d.ts
@@ -1,7 +1,7 @@
 import type { App } from 'vue';
-import DynamicRemoteLoader from './DynamicRemoteComponentLoader.vue';
+import DynamicRemoteComponentLoader from './DynamicRemoteComponentLoader.vue';
 declare const _default: {
     install(app: App): void;
 };
 export default _default;
-export { DynamicRemoteLoader };
+export { DynamicRemoteComponentLoader };
